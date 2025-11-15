32f1be65ac9bb7c200093ad6e3081b0423e379a4
toimurhasan <2imur.hasan@gmail.com>
2025-11-15 20:01:09 +0600

    banner added

--- src/components/pages/home/Hero.tsx
+++ src/components/pages/home/Hero.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  return <img src="banner.png" alt="banner-img" />;
+};
 
-export default Hero
+export default Hero;
--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import CategoryBar from "@/components/shared/CategoryBar/CategoryBar";
 import Navbar from "@/components/shared/Navbar/Navbar";
 import React from "react";
+import Hero from "./Hero";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <CategoryBar />
+      <Hero />
     </>
   );
 };
