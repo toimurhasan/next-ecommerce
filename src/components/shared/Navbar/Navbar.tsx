c991f663336ffd65ffdc6c06d846b87cf67dfadd
toimurhasan <2imur.hasan@gmail.com>
2025-11-18 13:14:19 +0600

    update

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import SearchBar from "./SearchBar";
 const Navbar = () => {
   const [search, setSearch] = useState("");
   return (
-    <div className="flex justify-between items-center py-3 lg:gap-12 flex-col lg:flex-row gap-2 px-4 lg:px-2">
+    <div className="flex justify-between items-center py-3 md:gap-6 lg:gap-12 flex-col md:flex-row gap-2 px-4 lg:px-2">
       <Logo />
       <SearchBar value={search} onChange={(e) => setSearch(e.target.value)} />
       <NavBtns />
